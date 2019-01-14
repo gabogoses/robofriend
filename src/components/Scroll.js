2e6123fc194b3235a8739804e8ce0b094df7dfd9
Gabriel Margoses <gabrielmargoses@gmail.com>
2019-01-14 17:27:53 +0100

    rearranging files

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from "react";
 
-const Scroll = (props) => {
-    return (
-        <div style={{ overflowY: 'scroll', height: '500px' }}>
-            {props.children}
-        </div>
-    );
+const Scroll = props => {
+  return (
+    <div style={{ overflowY: "scroll", height: "500px" }}>{props.children}</div>
+  );
 };
 
 export default Scroll;
